a90ec6322b585dc393cca963bb42c5609ff66b5c
cadenroberts <130518994+cadenroberts@users.noreply.github.com>
2025-11-22 20:54:00 -0800

    Refactor micromamba initialization in env.sh

--- westpa_template/env.sh
+++ westpa_template/env.sh
@@ -4,21 +4,16 @@ module load cudatoolkit
 
 export TMPDIR=$PSCRATCH
 mkdir -p $TMPDIR
-
-# >>> mamba initialize >>>
-# !! Contents within this block are managed by 'micromamba shell init' !!
 export MAMBA_EXE='/global/homes/c/cawrober/micromamba/micromamba'
 export MAMBA_ROOT_PREFIX='/global/homes/c/cawrober/micromamba'
 __mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__mamba_setup"
 else
-    alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
+    alias micromamba="$MAMBA_EXE"
 fi
 unset __mamba_setup
-#eval "$($MAMBA_EXE shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX")"
 micromamba activate --prefix /global/cfs/cdirs/m4229/caden/micromamba_root/envs/westpa_env
-# <<< mamba initialize <<<
 
 export HDF5_USE_FILE_LOCKING=0
 export MPI=1
